f8699eb64acfcdc111c819b68bb0e1c148c9ee3c
jfg-echols <jfg.echols@gmail.com>
2018-03-17 19:24:25 -0700

    figured out the variables

--- scraper.py
+++ scraper.py
@@ -55,23 +55,156 @@ for nation in DDNationDivs:
         shiparray['shiptier'] = perfdiv.text.split(' | ')[2]
         
         #gets general, armament, toobs, maneuverability, concealment
-        shipstats = thisDDsoup.find_all('div',class_='gw-popup-card b-tech-nav_item__opened')
+        shipstats = thisDDsoup.find('div',class_='b-performance_border').find_all('div',class_='gw-popup-card b-tech-nav_item__opened')
         for statgroup in shipstats:
             groupName = statgroup.find('div',class_='b-performance_title gw-popup-card_head js-tech-nav_head').text
-            # print('--')
-            # print(groupName)
-            # print('--')
+            print('--')
+            print(groupName)
+            print('--')
             stats = statgroup.find('div',class_='gw-popup-card_content').find_all('tr')
-            for stat in stats:
+            
+            ###GENERAL STAT GROUP
+
+            if groupName == 'General':
+                
+                shipCurrency = ''
+                shipCost = ''
+                shipHitPoints = ''
+                shipResearch = ''
+                for stat in stats:
+                    print('--------------')
+                    ##TODO - strip whitespace from variables                    
+                    if stat.find('span',class_='t-performance_left').text == 'Purchase price':
+                        shipCurrency = stat.find('img')['alt']
+                        shipCost = stat.find('span',class_='t-performance_right').text
+                    if stat.find('span',class_='t-performance_left').text == 'Hit Points':
+                        shipHitPoints = stat.find('span',class_='t-performance_right').text
+                    if stat.find('span',class_='t-performance_left').text == 'Research price':
+                        shipResearch = stat.find('span',class_='t-performance_right').text
+                    
+                if shipCost != 'promo':
+                    print('Currency: '+shipCurrency)
+                print('Cost: '+shipCost)
+                print('HP: '+shipHitPoints)
+                if shipResearch != '':
+                    print('ResearchXP: '+shipResearch)
+                #print(stats)
+            
+            ###MAIN BATTERY
+
+            elif groupName == 'Main Battery':
+                shipMainGun = ''
+                shipMainGunCount = ''
+                shipMainGunROF = ''
+                shipMainGunReload = ''
+                shipMainGunRotate = ''
+                shipMainGun180 = ''
+                shipMainGunRange = ''
+                shipMainGunDispersion = ''
+                shipMainGunHEShell = ''
+                shipMainGunHEDam = ''
+                shipMainGunHEFire = ''
+                shipMainGunHEVel = ''
+                shipMainGunHEWeight = ''
+                shipMainGunAPShell = ''
+                shipMainGunAPDam = ''
+                shipMainGunAPVel = ''
+                shipMainGunAPWeight = ''
+
+                shipMainGun = stats[0].find('span',class_='t-performance_left').text
+                shipMainGunCount = stats[0].find('span',class_='t-performance_right').text
+
+                for stat in stats:
+                    if stat.find('span',class_='t-performance_left').text == 'Rate of Fire':
+                        shipMainGunROF = stat.find('span',class_='t-performance_right').text
+                    if stat.find('span',class_='t-performance_left').text == 'Reload Time':
+                        shipMainGunReload = stat.find('span',class_='t-performance_right').text
+                    if stat.find('span',class_='t-performance_left').text == 'Rotation Speed':
+                        shipMainGunRotate = stat.find('span',class_='t-performance_right').text
+                    if stat.find('span',class_='t-performance_left').text == '180 Degree Turn Time':
+                        shipMainGun180 = stat.find('span',class_='t-performance_right').text
+                    if stat.find('span',class_='t-performance_left').text == 'Firing Range':
+                        shipMainGunRange = stat.find('span',class_='t-performance_right').text
+                    if stat.find('span',class_='t-performance_left').text == 'Maximum Dispersion':
+                        shipMainGunDispersion = stat.find('span',class_='t-performance_right').text
+                    if stat.find('span',class_='t-performance_left').text == 'HE Shell':
+                        shipMainGunHEShell = stat.find('span',class_='t-performance_right').text
+                    if stat.find('span',class_='t-performance_left').text == 'Maximum HE Shell Damage':
+                        shipMainGunHEDam = stat.find('span',class_='t-performance_right').text
+                    if stat.find('span',class_='t-performance_left').text == 'Chance of Fire on Target Caused by HE Shell':
+                        shipMainGunHEFire = stat.find('span',class_='t-performance_right').text
+                    if stat.find('span',class_='t-performance_left').text == 'Initial HE Shell Velocity':
+                        shipMainGunHEVel = stat.find('span',class_='t-performance_right').text
+                    if stat.find('span',class_='t-performance_left').text == 'HE Shell Weight':
+                        shipMainGunHEWeight = stat.find('span',class_='t-performance_right').text
+                    if stat.find('span',class_='t-performance_left').text == 'AP Shell':
+                        shipMainGunAPShell = stat.find('span',class_='t-performance_right').text
+                    if stat.find('span',class_='t-performance_left').text == 'Maximum AP Shell Damage':
+                        shipMainGunAPDam = stat.find('span',class_='t-performance_right').text
+                    if stat.find('span',class_='t-performance_left').text == 'Initial AP Shell Velocity':
+                        shipMainGunAPVel = stat.find('span',class_='t-performance_right').text
+                    if stat.find('span',class_='t-performance_left').text == 'AP Shell Weight':
+                        shipMainGunAPWeight = stat.find('span',class_='t-performance_right').text
+                
+                print(shipMainGun)
+                print(shipMainGunCount)
+                print(shipMainGunROF)
+                print(shipMainGunReload)
+                print(shipMainGunRotate)
+                print(shipMainGun180)
+                print(shipMainGunRange)
+                print(shipMainGunDispersion)
+                print(shipMainGunHEShell)
+                print(shipMainGunHEDam)
+                print(shipMainGunHEFire)
+                print(shipMainGunHEVel)
+                print(shipMainGunHEWeight)
+                print(shipMainGunAPShell)
+                print(shipMainGunAPDam)
+                print(shipMainGunAPVel)
+                print(shipMainGunAPWeight)
+            ##TODO - Secondary Armament - multiple Secondaries
+
+            elif groupName == 'Torpedo Tubes':
+                shipTorpTube = ''
+                shipTorpCount = ''
+                shipTorpReload = ''
+                shipTorpRorate = ''
+                shipTorp180 = ''
+                shipTorp = ''
+                shipTorpDam = ''
+                shipTorpSpeed = ''
+                shipTorpRange = ''
+
+            elif groupName == 'AA Defense':
+                shipAA = ''
+                shipAACount = ''
+                shipAADps = ''
+                shipAArange = ''
+
+            ##TODO - multiple AA Defense
+            elif groupName == 'Maneuverability':
+                shipSpeed = ''
+                shipTurnRadius = ''
+                shipRudderShift = ''
+
+
+            elif groupName == 'Concealment':
+                shipSurfaceDetect = ''
+                shipAirDetect = ''
+
+        
+        #     stats = statgroup.find('div',class_='gw-popup-card_content').find_all('tr')
+        #     for stat in stats:
                
-               shiparray[stat.find_all('span')[0].text] = stat.find_all('span')[1].text
+        #        shiparray[stat.find_all('span')[0].text] = stat.find_all('span')[1].text
         
         
         
         
         
         
-        print(shiparray)
+        # print(shiparray)
         input("Press a key to continue...")        
         # researchprice = 
         # purchaseprice =
